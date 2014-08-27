c7fad938055de8ce36186dbf0f9cd533d7105129
Donald Jones <donald@getplatypi.com>
2014-08-26 19:40:42 -0500

    Initial commit. Fixed reverse animation after timeout.

---
+++ css3animationtest/menu.ts
@@ -0,0 +1,26 @@
+﻿window.onload = () => {
+    var hex = document.getElementsByClassName('hex');
+
+    [].forEach.call(document.querySelectorAll('.hex'), function (el) {
+        el.addEventListener('mouseover', () => {
+            if (!el.classList.contains('hex-selected')) {
+                el.classList.add('hex-spin');
+                el.classList.add('hex-selected');   
+            }
+        }, false);
+
+        el.addEventListener('mouseleave', () => {
+            if (!el['timeout']) {
+                el['timeout'] = window.setTimeout(() => {
+                    el.classList.add('hex-spin');
+                    el['timeout'] = undefined;
+                    el.classList.remove('hex-selected');
+                }, 5000);
+            }
+        }, false);
+
+        el.addEventListener('webkitAnimationEnd', () => {
+            el.classList.remove('hex-spin');
+        }, false);
+    });
+};
